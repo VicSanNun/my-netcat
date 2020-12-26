6c63e467404a6eacb82c53154506ae0867e5fe3e
Victor Nunes <victornunes470@gmail.com>
2020-12-25 22:34:50 -0300

    client_sender function

--- main.py
+++ main.py
@@ -25,6 +25,37 @@ def instruction():
     print("echo 'ABCDEFG' | ./vsnnet.py -t 192.168.2.1 -p 100")
     sys.exit(0)
 
+def client_sender(buffer):
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        client.connect((target, port))
+        
+        if len(buffer):
+            client.send(buffer)
+
+        while True:
+            recv_len = 1
+            response = ""
+
+            while recv_len:
+                data = client.recv(4096)
+                recv_len = len(data)
+                response+=data
+
+                if recv_len < 4096:
+                    break
+
+            print(response)
+
+            buffer = raw_input("")
+            buffer+= "\n"
+
+            client.send(buffer)
+    except:
+        print("[*] Exception! Exiting.")
+        client.close()
+
 def main():
     global listen
     global port
